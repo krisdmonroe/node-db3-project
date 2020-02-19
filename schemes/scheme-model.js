b4e3b8b0b3a01c2ee81f4a2f3af4e42f409b84ec
Kris Monroe <krisdmonroe@gmail.com>
2020-02-19 15:45:09 -0800

    Finished Half

---
+++ schemes/scheme-model.js
@@ -0,0 +1,30 @@
+const db = require("./dbCofig.js")
+
+module.exports = {
+
+}  
+
+function find() {
+    
+  }
+  
+  function findById(id) {
+    
+  }
+  
+  function findSteps(id) {
+   
+  }
+  
+  function add(scheme) {
+ 
+  }
+  
+  function update(changes, id) {
+  
+  }
+  
+  function remove(id) {
+    
+  }
+  
